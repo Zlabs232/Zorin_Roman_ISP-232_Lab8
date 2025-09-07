5ec69635c0a7e43bfcb015a8fb38c8ed4f298fe1
Роман Зорин <sgena170315@gmail.com>
2025-09-07 18:31:50 +0300

    Задание 5 — Использование глобального пространства имён

---
+++ 5_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _5_Task
+{
+    class System
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hello");
+            //Ошибка, потому что есть глобал System из .NET и в наш клас System
+            global::System.Console.WriteLine("Hello World!");
+
+            System mySystem = new System(); 
+            mySystem.MyMethod();
+        }
+    }
+}
