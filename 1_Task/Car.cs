8f529df2e55806eff48e57872a3ad2681cea589e
Роман Зорин <sgena170315@gmail.com>
2025-09-07 18:10:36 +0300

    Задание 1 — Класс «Автомобиль»

---
+++ 1_Task/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace _1_Task
+{
+    internal class Car
+    {
+        private string Brand {  get; set; }
+        private string Model { get; set; }
+        private int Year { get; set; }
+        private int Mileage { get; set; } 
+
+        public Car(string brand, string model, int year, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Бренд: {Brand}, Модель: {Model}, Год выпуска: {Year}, Пробег: {Mileage}");
+        }
+    }
+}
---
+++ 1_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new("Toyota", "S35", 1945, 1830);
+            myCar.PrintInfo();
+            Car myCar2 = new("Mazda", "9dj8F", 2022, 1490);
+            myCar2.PrintInfo();
+        }
+    }
+}
