b244dabb778a817bcd753626a32950e2a73aa79e
Роман Зорин <sgena170315@gmail.com>
2025-09-07 18:17:56 +0300

    Задание 3 — Работа с пространствами имён

---
+++ 3_Task/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp
+{
+    internal class User
+    {
+        public string name {  get; set; }
+        public string email { get; set; }
+        public bool isActive { get; set; }
+
+        public void print() => Console.WriteLine($"Имя: {name}, почта: {email}, В сети: {(isActive == true ? "Да" : "Нет")}");
+    }
+}
---
+++ 3_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyApp;
+namespace _3_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User me = new User();
+            me.name = "Roman";
+            me.email = "z0rin.romanchik@yandex.ru";
+            me.isActive = true;
+
+            me.print();
+        }
+    }
+}
