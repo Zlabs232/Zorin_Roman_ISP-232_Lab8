0f1ef55f8e05eeabbd28e22ba450099127ecd073
Роман Зорин <sgena170315@gmail.com>
2025-09-07 17:41:46 +0300

    Пример работы со структурой и констуктором в ней

---
+++ struct/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace struct2;
+internal class Program
+{
+    struct Person
+    {
+        public string name; public int age;
+
+        public Person(string name = "Том", int age = 1)
+        {
+            this.name = name; this.age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Person garen; 
+        garen.name = "Гарен";
+        garen.age = 25;
+        garen.Print();
+    }
+}
