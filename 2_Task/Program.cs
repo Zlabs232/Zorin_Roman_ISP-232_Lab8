a83c018d48c14271bb035b97e84f1837b7dd19b9
Роман Зорин <sgena170315@gmail.com>
2025-09-07 18:12:44 +0300

    Задание 2 — Структура «Точка на плоскости»

---
+++ 2_Task/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _2_Task
+{
+    internal class Program
+    {
+        public struct Point2D
+        {
+            public int X;
+            public int Y;
+
+            public Point2D(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public double DistanceTo(Point2D other)
+            {
+                int dx = X - other.X;
+                int dy = Y - other.Y;
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var p1 = new Point2D(10, 30);
+            var p2 = new Point2D(13, 24);
+            Console.WriteLine(p1.DistanceTo(p2));
+
+        }
+    }
+}
