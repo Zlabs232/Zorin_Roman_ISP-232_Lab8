bca098268bcc4e2d7e3a820151b09c8998043d75
Роман Зорин <sgena170315@gmail.com>
2025-09-07 18:26:11 +0300

    Задание 4 — Проверка типа значений

---
+++ 4_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _4_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueChecker checker = new ValueChecker();
+            checker.CheckType(5);
+            checker.CheckType("Hello");
+        }
+    }
+}
---
+++ 4_Task/ValueChecker.cs
@@ -0,0 +1,13 @@
+﻿namespace _4_Task
+{
+    internal class ValueChecker
+    {
+        public void CheckType(object value)
+        {
+            string typeName = value.GetType().Name;
+            string category = value is ValueType ? "значимый" : "ссылочный";
+
+            Console.WriteLine($"{category} тип ({typeName})");
+        }
+    }
+}
