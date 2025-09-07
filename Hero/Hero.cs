a67885049b29ee6d6ad086064290975d49178fe6
Роман Зорин <sgena170315@gmail.com>
2025-09-07 17:32:01 +0300

    второй мини-проект – Персонаж.

---
+++ Hero/Hero.cs
@@ -0,0 +1,62 @@
+﻿namespace Hero
+{
+    internal class Hero
+    {
+        public string Name { get; set; }
+        public int Mana { get; set; }
+        public int Level { get; set; }
+
+        private int _health;
+        private int _maxHealth;
+
+        public Hero(string name, int maxHealth)
+        {
+            Name = name;
+            _maxHealth = maxHealth;
+            _health = maxHealth;
+            Mana = 100;
+            Level = 1;
+        }
+
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                if (value < 0)
+                    _health = 0;
+                else if (value > _maxHealth)
+                    _health = _maxHealth;
+                else
+                    _health = value;
+            }
+        }
+
+        public string Status => _health > 0 ? "Жив" : "Сдох";
+
+        public void TakeDamage(int damage)
+        {
+            if (_health <= 0)
+            {
+                Console.WriteLine($"{Name} уже сдох и не может получать урон");
+                return;
+            }
+            Health -= damage;
+            Console.WriteLine($"{Name} получает {damage} урона. HP: {Health} | Статус: {Status}");
+            if (_health <= 0)
+                Console.WriteLine($"{Name} сдох!");
+        }
+
+        public void Heal(int amount)
+        {
+            if(_health <= 0)
+            {
+                Console.WriteLine($"{Name} погиб, он не может быть исцелен");
+                return;
+            }
+            Health += amount;
+            Console.WriteLine($"{Name} исцелен на {amount}. HP: {Health} | Статус: {Status}");
+
+        }
+    }
+}
---
+++ Hero/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Боб", 120);
+            Console.WriteLine($"Имя героя: {hero.Name}\nУровень героя: {hero.Level}\nМана: {hero.Mana}\nСтатус: {hero.Status}");
+
+            hero.TakeDamage(30);
+            hero.Heal(30);
+            hero.TakeDamage(3000);
+            hero.Heal(50);
+        }
+    }
+}
